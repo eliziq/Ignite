fa9d2833d16b3c1c65a94f53078811e96abd511a
Lizik <elizavetamomsen@gmail.com>
2021-11-22 17:00:30 +0200

    API SetUp

---
+++ src/api.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+//BASE URL
+const base_url = "https://api.rawg.io/api/";
+const key = `&key=${process.env.REACT_APP_RAWG_API}`;
+
+//getting the date
+const getCurrentMonth = () => {
+	const month = new Date().getMonth() + 1;
+	if (month < 10) {
+		return `0${month}`;
+	} else {
+		return month;
+	}
+};
+const getCurrentDay = () => {
+	const day = new Date().getDate();
+	if (day < 10) {
+		return `0${day}`;
+	} else {
+		return day;
+	}
+};
+//current day/month/year
+const currentDay = getCurrentDay();
+const currentMonth = getCurrentMonth();
+const currentYear = new Date().getFullYear();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//popular games
+const popularGames = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+const popularGamesURL = () => {
+	return `${base_url}${popularGames}${key}`;
+};
+console.log(popularGamesURL());
